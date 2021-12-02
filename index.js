d6cc4bb2101ffeec9d0b56c1d031b1dc2f611052
kosi2109 <nolockfacebook55@gmail.com>
2021-12-02 22:47:30 +0630

    final

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const errorHandaler = require("./helper/error-handaler");
 //   }
   
 // middleware
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(errorHandaler);
